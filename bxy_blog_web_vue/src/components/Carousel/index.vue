d6667a5c2cf66f0b9c71edc9257d7fe27f145bd8
beloved-zh <1425279634@qq.com>
2020-08-03 17:55:57 +0800

    首页

---
+++ bxy_blog_web_vue/src/components/Carousel/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <el-carousel indicator-position="outside" height="450px">
+      <el-carousel-item v-for="(item,index) in blogList" :key="index">
+        <el-image :src="item.url" fit="fill"></el-image>
+        <div class="carousel-title">
+          <span>{{item.title}}</span>
+        </div>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Carousel',
+  data() {
+    return {
+      blogList: [
+        {
+          url: 'http://qd4zqd4qv.bkt.clouddn.com/0d68c626f8e94a4287d15273b034fde4.jpg',
+          title: 'Docker'
+        },
+        {
+          url: 'http://qd4zqd4qv.bkt.clouddn.com/1b5adba89e8746c5b51aebd4d0504ea8.jpg',
+          title: 'Github'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.carousel-title {
+  cursor: pointer;
+  position: absolute;
+  z-index: 10;
+  bottom: 40px;
+  height: 40px;
+  width: 100%;
+  line-height: 40px;
+  text-align: center;
+  background: rgba(0, 0, 0, 0.3);
+}
+.carousel-title span {
+  color: white;
+  font-size: 22px;
+  display: inline-block;
+}
+</style>
--- bxy_blog_web_vue/src/layout/components/Footer/index.vue
+++ bxy_blog_web_vue/src/layout/components/Footer/index.vue
@@ -1,20 +1,5 @@
 <template>
     <div id="layout-footer">
-        <div class="footer-main">
-            <!-- <div class="footer-item" v-if="socials.length">
-                <div v-for="item in socials" :key="item.id"><a target="_blank" class="out-link" :href="item.href"><i class="iconfont" :class="item.icon"></i>{{item.title}}</a></div>
-            </div> -->
-            <!-- <div class="footer-item">
-                <div style="font-size:17px;font-weight: bold;">资源</div>
-                <div><a target="_blank" class="out-link" href="https://segmentfault.com/weekly?utm_source=sf-footer">每周精选</a></div>
-                <div><a target="_blank" class="out-link" href="https://www.jmjc.tech/">简明教程</a></div>
-                <div><a target="_blank" class="out-link" href="https://www.liaoxuefeng.com/">廖雪峰的官方网站</a></div>
-            </div>
-            <div class="footer-item">
-                <div>本站已苟活 {{runTimeInterval}}</div>
-                <div><a target="_blank" class="out-link" href="#">☞后台管理</a></div>
-            </div> -->
-        </div>
         <div class="copyright">Copyright © 2020 by <a target="_blank" class="out-link" href="https://www.beloved.ink">beloved.ink</a> . All rights reserved. | <a target="_blank" class="out-link" href="http://www.beian.miit.gov.cn">陕ICP备19025605号-1</a></div>
     </div>
 </template>
@@ -45,22 +30,6 @@
     color: #9c9c9c;
     a.out-link:hover{
         color: #ff6d6d;
-    }
-    .footer-main{
-        max-width: 800px;
-        margin: 0 auto 20px auto;
-        display: flex;
-        justify-content: space-around;
-        align-items: flex-start;
-        .footer-item{
-            flex: 1;
-            & > div:not(:last-child){
-                margin-bottom: 10px;
-            }
-            i{
-                margin-right: 10px;
-            }
-        }
     }
     .copyright{
         text-align: center;
